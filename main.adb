ee6159796aae7a95ed8e898e65ba9f28ed9860e0
Akutchi <hugo.buffler@ecole.ensicaen.fr>
2024-02-10 18:28:39 +0100

    initial commit

---
+++ main.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure main is
+
+begin
+
+    Put_Line ("This is the main program");
+
+end main;
---
+++ operations.ads
@@ -0,0 +1,20 @@
+package operations is
+
+    type BinaryOp is (
+
+        GREATER_THAN,
+        GREATER_EQUAL,
+        LESS_THAN,
+        LESS_EQUAL,
+        EQUAL,
+        DIFFERENT
+    );
+
+    type UnaryOp is (
+
+        INVERSE,
+        MINUS
+    );
+
+
+end operations;
